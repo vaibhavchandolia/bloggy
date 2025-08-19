76616c38f68da99e86b9e961fc42e997c82eb72d
Vaibhav Chandolia <vaibhavchandolia52@gmail.com>
2025-08-20 00:36:48 +0530

    addde clerk auth

---
+++ src/main.jsx
@@ -0,0 +1,58 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Homepage from './routes/Homepage.jsx'
+import PostListPage from './routes/PostListPage.jsx'
+import Write from './routes/Write.jsx'
+import Login from './routes/Login.jsx'
+import SinglePostPage from './routes/SinglePostPage.jsx'
+import RegisterPage from './routes/RegisterPage.jsx'
+import MainLayout from './layouts/MainLayout.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    children : [
+      {
+        path: '/',
+        element: <Homepage />
+      },
+      {
+        path: '/posts',
+        element: <PostListPage/>
+      },
+      {
+        path: '/:slug',
+        element: <SinglePostPage />
+      },
+      {
+        path: '/write',
+        element: <Write />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/register',
+        element: <RegisterPage />
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <RouterProvider router={router} />
+    </ClerkProvider>
+  </StrictMode>,
+)
---
+++ src/routes/RegisterPage.jsx
@@ -0,0 +1,7 @@
+const RegisterPage = () => {
+  return (
+    <div className=''>RegisterPage</div>
+  )
+}
+
+export default RegisterPage
