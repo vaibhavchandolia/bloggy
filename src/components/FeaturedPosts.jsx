fbb05f72a42a4413830c03d60a9ea12ee05a6bac
Vaibhav Chandolia <vaibhavchandolia52@gmail.com>
2025-08-27 00:07:58 +0530

    feature ui complete

--- src/components/FeaturedPosts.jsx
+++ src/components/FeaturedPosts.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import Img from "./Image"
 
 const FeaturedPosts = () => {
@@ -8,10 +9,68 @@ const FeaturedPosts = () => {
             {/* Image */}
             <Img src="featured1.jpeg" classname="rounded-3xl object-cover" />
             {/* details */}
+            <div className="flex items-center gap-4">
+              <h1 className="font-semibold items-center gap-4">01.</h1>
+              <Link className="text-blue-800 lg:text-lg">Web design</Link>
+              <span className="text-gray-500">2 days ago</span>
+            </div>
             {/* title */}
+            <Link to="/test" className="text-xl lg:text-3xl font-semibold lg:font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</Link>
         </div>
         {/* other posts */}
-        <div className=""></div>
+        <div className="w-full lg:w-1/2 flex flex-col gap-4">
+          {/* second */}
+          <div className="lg:h-1/3 flex justify-between gap-4">
+            <Img src="featured2.jpeg" classname="rounded-3xl object cover w-1/3 aspect-video" />
+            {/* details & title */}
+            <div className="w-2/3">
+              {/* details  */}
+              <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
+                <h1 className="font-semibold">02.</h1>
+                <Link className="text-blue-800">Web Design</Link>
+                <span className="text-gray-500 text-sm">2 days ago</span>
+              </div>
+              {/* title  */}
+              <Link to="/test" className="text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              </Link>
+            </div>
+          </div>
+          {/* third */}
+          <div className="lg:h-1/3 flex justify-between gap-4">
+            <Img src="featured2.jpeg" classname="rounded-3xl object cover w-1/3 aspect-video" />
+            {/* details & title */}
+            <div className="w-2/3">
+              {/* details  */}
+              <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
+                <h1 className="font-semibold">02.</h1>
+                <Link className="text-blue-800">Web Design</Link>
+                <span className="text-gray-500 text-sm">2 days ago</span>
+              </div>
+              {/* title  */}
+              <Link to="/test" className="text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              </Link>
+            </div>
+          </div>
+          {/* fourth */}
+          <div className="lg:h-1/3 flex justify-between gap-4">
+            <Img src="featured2.jpeg" classname="rounded-3xl object cover w-1/3 aspect-video" />
+            {/* details & title */}
+            <div className="w-2/3">
+              {/* details  */}
+              <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
+                <h1 className="font-semibold">02.</h1>
+                <Link className="text-blue-800">Web Design</Link>
+                <span className="text-gray-500 text-sm">2 days ago</span>
+              </div>
+              {/* title  */}
+              <Link to="/test" className="text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              </Link>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
