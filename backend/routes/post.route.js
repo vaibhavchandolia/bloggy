1ea6de5044688cf65ba6568908a386d9728d54af
vaibhavchandolia <vaibhavchandolia52@gmail.com>
2025-09-06 18:26:30 +0000

    working on mongodb connection

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,8 +1,11 @@
 import express from "express"
+// import Post from "../models/post.model"
 
 const router = express.Router()
 
-router.get("/", (req,res)=>{
+router.get("/", async (req,res)=>{
+
+    // const posts = await Post.find()
     res.send("Hello Postman")
 })
 
