58a3d85aaac58db4477ea4d590eca461a16d17f5
vaibhav chandolia <vaibhavchandolia52@gmail.com>
2025-12-05 01:13:24 +0530

    working on comment integration

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -1,11 +1,52 @@
 import Comment from "../models/comment.model.js";
+import User from "../models/user.model.js";
 
 export const getPostComments = async (req, res) => {
   const comments = await Comment.find({ post: req.params.postId })
     .populate("user", "username img")
     .sort({ createdAt: -1 });
-  
-  res.json(comments)
+
+  res.json(comments);
 };
-export const addComment = async (req, res) => {};
-export const deleteComment = async (req, res) => {};
+
+export const addComment = async (req, res) => {
+  const clerkUserId = req.auth.user.id;
+  const postId = req.params.postId;
+
+  if (!clerkUserId) {
+    return res.status(401).json("Not authenticated!");
+  }
+
+  const user = User.findOne({ clerkUserId });
+
+  const newComment = new Comment({
+    ...req.body,
+    user: user._id,
+    posts: postId,
+  });
+
+  const savedComment = await newComment.save();
+
+  res.status(201).json(savedComment);
+};
+export const deleteComment = async (req, res) => {
+  const clerkUserId = req.auth.user.id;
+  const id = req.params.idd;
+
+  if (!clerkUserId) {
+    return res.status(401).json("Not authenticated!");
+  }
+
+  const user = User.findOne({ clerkUserId });
+
+  const deletedComment = await Comment.findOneAndDelete({
+    _id: id,
+    user: user._id,
+  });
+  
+  if(!deletedComment){
+    return res.status(403).json("You can only delete only your comment!")
+  }
+  
+  res.status(200).json("Comment deleted successfully");
+};
--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,6 +1,8 @@
 import Comment from "./Comment";
 import axios from "axios";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useAuth } from "@clerk/clerk-react";
+import { toast } from "react-toastify";
 
 const fetchComments = async (postId) => {
   const res = await axios.get(
@@ -11,26 +13,65 @@ const fetchComments = async (postId) => {
 };
 
 const Comments = ({ postId }) => {
+  const { getToken } = useAuth();
+
   const { isPending, error, data } = useQuery({
     queryKey: ["comments", postId],
     queryFn: () => fetchComments(postId),
   });
 
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: async (newComment) => {
+      const token = await getToken();
+      return axios.post(
+        `${import.meta.env.VITE_API_URL}/comments/${postId}`,
+        newComment,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["comments", postId] });
+    },
+    onError: (error) => {
+      toast.error(error.response.data);
+    },
+  });
+
   if (isPending) return "Loading...";
   if (error) return "Something went wrong" + error.message;
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const data = {
+      desc: formData.get("desc"),
+    };
+    mutation.mutate(data);
+  };
 
   return (
     <div className="flex flex-col gap-8 lg:w-3/5">
       <h1 className="text-xl text-gray-500 underline">Comments</h1>
-      <div className="flex items-center justify-between gap-8 w-full">
+      <form
+        onSubmit={handlesubmit}
+        className="flex items-center justify-between gap-8 w-full"
+      >
         <textarea
+          name="desc"
           placeholder="Write a comment.."
           className="w-full p-4 rounded-xl"
         />
         <button className="bg-blue-800 px-4 py-3 text-white font-medium rounded-xl">
           Send
         </button>
-      </div>
+      </form>
       {data.map((comment) => {
         <Comment key={comment._id} comment={comment} />;
       })}
